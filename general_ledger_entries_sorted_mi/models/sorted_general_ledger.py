aadb6a21b09a6fbbf59f2bab546120efd7272181
manoj.khadka <manojlkdka@gmail.com>
2022-03-03 16:15:44 +0545

    balance bug fix

--- general_ledger_entries_sorted_mi/models/sorted_general_ledger.py
+++ general_ledger_entries_sorted_mi/models/sorted_general_ledger.py
@@ -10,5 +10,12 @@ class GeneralViewInherit(models.TransientModel):
     def _get_report_values(self, data):
         res = super(GeneralViewInherit,self)._get_report_values(data)
         for acc in res['Accounts']:
-            acc['move_lines'].sort(key=lambda l:l['ldate']) 
+            
+            acc['move_lines'].sort(key=lambda l:l['ldate'])
+            balance = 0.0
+            # Recompute balance again after sorting datewise
+            for i, line in enumerate(acc['move_lines']):
+                balance += round(line['debit'], 2) - round(line['credit'], 2)
+                acc['move_lines'][i]['balance'] = balance
+
         return res
