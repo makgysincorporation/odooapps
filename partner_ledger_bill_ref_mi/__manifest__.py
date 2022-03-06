3bf92b3ec062ba86c645062940146c0b469ba028
manoj.khadka <manojlkdka@gmail.com>
2022-03-06 20:20:04 +0545

    partner_ledger_bill_ref_mi

---
+++ partner_ledger_bill_ref_mi/__manifest__.py
@@ -0,0 +1,24 @@
+
+{
+    'name': 'Partner Ledger Bill Reference',
+    'summary': 'Adds bill reference View in Partner Ledger',
+    'description': '''
+This module provide extra field bill reference in in partner ledger's move lines ''',
+    'author': 'Makgys Incorporation Pvt. Ltd.',
+    'version': '14.0.1.0.0',
+    'category': 'Accounting/Accounting',
+    'depends': [
+        'dynamic_accounts_report'
+    ],
+    'data': [
+    ],
+    'qweb': [
+        'static/src/xml/partner_ledger_bill_ref.xml',
+    ],
+     'license': 'OPL-1',
+    'images': ['static/description/Banner.png'],
+    'price': 13.0,
+    'currency': 'EUR',
+    'installable': True,
+    'application': False,
+}
