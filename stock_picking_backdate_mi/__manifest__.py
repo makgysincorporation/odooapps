259516ae3acdcf6bcbeb729906558001ae80c15e
manoj.khadka <manojlkdka@gmail.com>
2022-03-10 20:16:52 +0545

    stock_picking_backdate_mi

---
+++ stock_picking_backdate_mi/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Stock Picking Backdate',
+    'version': '14.0.1.0.0',
+    'category': 'Extra Tools',
+    'author': 'Makgys Incorporation Pvt. Ltd.',
+    'summary': 'assigning backdate to picking',
+    'depends': [
+        'stock','account'
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'wizard/change_to_backdate_wiz.xml',
+        'views/stock_picking_backdate_action.xml'
+    ],
+    'license': 'OPL-1',
+    'images': ['static/description/Banner.png'],
+    'price': 65.0,
+    'currency': 'EUR',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
